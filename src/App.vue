7b09dc12dab6aec89a82254f8aaab5b847acb647
Rickard <102632224+locorickardo@users.noreply.github.com>
2025-02-19 10:35:06 +0100

    Vue setup, Navbar added

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<template>
+  <NavBar />
+  <Footer />
+  <RouterView />
+  <RouterLink />
+</template>
+
+<style scoped>
+
+</style>
